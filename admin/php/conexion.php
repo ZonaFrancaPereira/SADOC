3c2893b9ab22b6ae28707040295b4a3e1976ec79
Ygarciazf <ygarcia@zonafrancadepereira.com>
2024-02-02 10:40:12 -0500

    02/02/2024

--- admin/php/conexion.php
+++ admin/php/conexion.php
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<?php
-$host = 'localhost'; // Puede ser localhost o proporcionado por Hostinguer
-$dbname = 'upload';
-$username = 'root';
-$password = '';
-=======
 <?php 
 	$conn = new PDO(
       'mysql:host=localhost;
@@ -14,7 +7,6 @@ $password = '';
 	try {
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->exec("set names utf8");
->>>>>>> be5e1c9beb83f70fdf62da8caf57c26b58afb055
 
 	} catch(PDOException $e) {
       echo $sql . "<br>" . $e->getMessage();
